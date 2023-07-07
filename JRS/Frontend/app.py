913c84118020e5947c177a2230b6a96c3992bebb
Rastra Dhoj Karki <rastradhoj@gmail.com>
2023-07-07 15:32:59 +0545

    the pickled load now

--- JRS/Frontend/app.py
+++ JRS/Frontend/app.py
@@ -4,8 +4,8 @@ import pickle
 
 dfPickle = open("../Backend/df.pkl", 'rb')
 similarityPickle = open("../Backend/similarity.pkl", 'rb')
-df = pickle.load(dfPickle)
-similarity = pickle.load(similarityPickle)
+df = pd.read_pickle(dfPickle)
+similarity = pd.read_pickle(similarityPickle)
 
 # Web App
 st.title('Job Recommendation System')
