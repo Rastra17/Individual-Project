104239a788a513baa5d826fc5938da4a33c4dab2
Rastra Dhoj Karki <rastradhoj@gmail.com>
2023-07-07 15:43:29 +0545

    working dummy frontend

--- JRS/Frontend/app.py
+++ JRS/Frontend/app.py
@@ -7,5 +7,21 @@ similarityPickle = open("../Backend/similarity.pkl", 'rb')
 df = pd.read_pickle(dfPickle)
 similarity = pd.read_pickle(similarityPickle)
 
+
+def recommendation(title):
+    placeIndex = df[df['Title'] == title].index[0]
+    placeIndex = df.index.get_loc(placeIndex)
+    distances = sorted(list(enumerate(similarity[placeIndex])),key=lambda x:x[1], reverse=False)[1:20]
+    
+    jobs = []
+    for i in distances:
+        jobs.append(df.iloc[i[0]].Title)
+    return jobs
+
 # Web App
 st.title('Job Recommendation System')
+title = st.selectbox('search job', df['Title'])
+jobs = recommendation(title)
+
+if jobs:
+    st.write(jobs)
